643e33cd7a794433a6f1ea2a831a7135446d3db4
usnatdinov02 <usnatdinov02@github.com>
2025-11-06 13:07:58 +0500

    Update project

---
+++ setting/local_settings.py
@@ -0,0 +1,9 @@
+ALLOWED_HOSTS = ['usnadtinov02.pythonanywhere.com']
+
+DEBUG = False
+
+STATIC_ROOT = '/home/usnadtinov02/mercedes_shop/static'
+STATIC_URL = '/static/'
+
+MEDIA_ROOT = '/home/usnadtinov02/mercedes_shop/media'
+MEDIA_URL = '/media/'
