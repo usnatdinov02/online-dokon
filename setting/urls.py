3d9f5dd118c1f6e1dfe356158e4960f5dc1ae6a6
usnatdinov02 <usnatdinov02@github.com>
2025-11-06 10:09:13 +0500

    first commit

--- setting/urls.py
+++ setting/urls.py
@@ -2,10 +2,20 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
+
+    # Informational pages (mobile-friendly)
+    path('about/', TemplateView.as_view(template_name='about.html'), name='about'),
+    path('help/', TemplateView.as_view(template_name='help-center.html'), name='help_center'),
+    path('shipping/', TemplateView.as_view(template_name='shipping.html'), name='shipping'),
+    path('returns/', TemplateView.as_view(template_name='returns.html'), name='returns'),
+    path('privacy/', TemplateView.as_view(template_name='privacy.html'), name='privacy'),
+    path('payments/', TemplateView.as_view(template_name='payments.html'), name='payments'),
+    path('contact-mobile/', TemplateView.as_view(template_name='contact-mobile.html'), name='contact_mobile'),
 ]
 
 # Media va static fayllar uchun URL
